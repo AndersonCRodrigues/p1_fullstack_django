3c2a437947be47e83cb238338b10b310d0e68090
andersoncrodrigues <anderson.c.rodrigues@hotmail.com>
2026-09-16 16:30:31 -0300

    feat(acervo): implementa views de listagem e cadastro de livros

---
+++ acervo/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import LivroForm
+from .models import Livro
+
+
+def inicio(request):
+    return HttpResponse("Olá, acervo!")
+
+
+def lista_livros(request):
+    livros = Livro.objects.all()
+    return render(request, "acervo/lista.html", {"livros": livros})
+
+
+def novo_livro(request):
+    if request.method == "POST":
+        form = LivroForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("lista")
+    else:
+        form = LivroForm()
+
+    return render(request, "acervo/form.html", {"form": form})
