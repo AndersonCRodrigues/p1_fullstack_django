864e991d07dda66fe6c84775d0d2fb35e9c4d942
Anderson Costa Rodrigues <anderson.c.rodrigues@hotmail.com>
2026-09-16 19:42:46 -0300

    feat: url update de disponibilidade

--- acervo/urls.py
+++ acervo/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.inicio),
     path("livros/", views.lista_livros, name="lista"),
     path("novo/", views.novo_livro, name="novo_livro"),
+    path("disponivel/<int:pk>/toggle", views.update_disponivel, name="toggle_disponivel"),
+
 ]
