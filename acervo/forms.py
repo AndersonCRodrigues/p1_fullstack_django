7729ed692961195bff0646ecdfad813318ada6c3
Anderson Costa Rodrigues <anderson.c.rodrigues@hotmail.com>
2026-09-16 20:13:40 -0300

    chore: adiciona tipo e categoria como choices

--- acervo/forms.py
+++ acervo/forms.py
@@ -6,4 +6,24 @@ from .models import Livro
 class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
-        fields = ("titulo", "autor", "ano")
+        fields = ("titulo", "autor", "ano", "tipo", "categoria")
+        widgets = {
+            "titulo": forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Título do livro'}
+            ),
+            "autor": forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Nome do autor'}
+            ),
+            'ano': forms.NumberInput( 
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: 2026',
+                'min': '1440',
+            }),
+            "tipo": forms.Select(
+                attrs={"class":"form-control"}
+            ),
+            'categoria': forms.Select(
+                attrs={'class': 'form-control'}
+            ),
+        }
