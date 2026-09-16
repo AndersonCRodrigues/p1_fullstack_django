236bcd1efd281fd5dd2ae8490e370cbcef35cca9
andersoncrodrigues <anderson.c.rodrigues@hotmail.com>
2026-09-16 16:30:31 -0300

    feat(acervo): define rotas do app acervo

---
+++ acervo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.inicio),
+    path("livros/", views.lista_livros, name="lista"),
+    path("novo/", views.novo_livro, name="novo_livro"),
+]
